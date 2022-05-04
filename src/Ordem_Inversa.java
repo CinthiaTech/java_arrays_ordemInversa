3ed6a22e923e2ba645e92b75a70e9ac12f422dfe
Cinthia Rodrigues Eller <cinthiaeller@gmail.com>
2022-05-04 14:11:52 -0300

    Bootcamp DIO GFT

---
+++ src/Ordem_Inversa.java
@@ -0,0 +1,18 @@
+public class Ordem_Inversa {
+    public static void main(String[] args) {
+        int[] vetor = {-5, -6, 15, 50, 8, 4};
+        //System.out.println(Vetor.length);
+
+        System.out.print("Vetor: ");
+        int count = 0;
+
+        while (count < (vetor.length)) {
+            System.out.print(vetor[count] + " ");
+            count++;
+        }
+
+        for (int i = (vetor.length - 1); i >= 0; i--) {
+            System.out.println(vetor[i] + " ");
+        }
+    }
+}
